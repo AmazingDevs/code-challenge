f0b6eb38c86277b3571b28b91f792d8f18f76337
Matheus Luiz Costa Freitas <matheus_freitas_ti@hotmail.com>
2022-07-23 21:55:10 -0300

    Adding the ShoppingCart microservice

---
+++ ShoppingCart/ShoppingCart/Services/StockService.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using ShoppingCart.Models;
+using ShoppingCart.Services.IService;
+using System.Text;
+
+namespace ShoppingCart.Services
+{
+	public class StockService : IStockService
+	{
+		public async Task<bool> ResquestMovement(Sale sale)
+		{
+			var stockData = sale.SaleItems.Select(x => new ProductStock(x.ProductId, x.Quantity));
+
+			var client = new HttpClient();
+			var myContent = JsonConvert.SerializeObject(stockData);
+			var httpContent = new StringContent(myContent, Encoding.UTF8, "application/json");
+
+			var result = await client.PatchAsync("https://localhost:7022/api/Stock", httpContent);
+
+			return result.IsSuccessStatusCode;
+		}
+	}
+}
